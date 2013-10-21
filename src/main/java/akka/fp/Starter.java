e1f1e86d069caee53e379fbaf7c68a3b527a45b8
bluecobalt27 <bluecobalt27@9e7fac7f-3ebd-4cb7-9aa0-261363cd0aaf>
2013-10-21 18:16:17 +0000

    camel integration started

---
+++ src/main/java/akka/fp/Starter.java
@@ -0,0 +1,34 @@
+package akka.fp;
+
+import akka.actor.ActorRef;
+import akka.camel.CamelMessage;
+import akka.camel.javaapi.UntypedConsumerActor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cobalt
+ * Date: 16.10.2013
+ * Time: 13:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class Starter extends UntypedConsumerActor {
+    private final ActorRef listener;
+
+    public Starter(ActorRef reader) {
+        this.listener = reader;
+    }
+
+    public String getEndpointUri() {
+        return "jetty:http://localhost:8877/camel/default";
+    }
+
+    public void onReceive(Object message) {
+        if (message instanceof CamelMessage) {
+            CamelMessage camelMessage = (CamelMessage) message;
+            String body = "Bla";
+            getSender().tell(String.format("Received message: %s",body), getSelf());
+            listener.tell(new FP.Curate(), getSelf());
+        } else
+            unhandled(message);
+    }
+}
