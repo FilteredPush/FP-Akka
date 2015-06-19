48d6c3715c7c291b049726117c81d2f978386f30
chicoreus <chicoreus@9e7fac7f-3ebd-4cb7-9aa0-261363cd0aaf>
2015-06-19 12:59:09 +0000

    ISSUE:   PURPOSE: FP-Akka, minor cleanup in JettyStarter.  DESCRIPTION:

--- trunk/FP-Akka/src/main/java/org/filteredpush/akka/workflows/jettyStarter.java
+++ trunk/FP-Akka/src/main/java/org/filteredpush/akka/workflows/jettyStarter.java
@@ -42,7 +42,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /*
 * @begin MongoWorkflow
