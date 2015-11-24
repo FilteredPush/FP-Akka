6640322429229db26b45a0d11be9ad9c52fba49d
dlowery <dlowery@9e7fac7f-3ebd-4cb7-9aa0-261363cd0aaf>
2015-11-24 21:32:22 +0000

    removing accidental check in of directory to wrong location

--- trunk/FP-Akka/fp-core/src/main/java/org/filteredpush/knowledge/TripleStore.java
+++
@@ -1,9 +0,0 @@
-package org.filteredpush.knowledge;
-
-/**
- * Created by lowery on 11/4/15.
- */
-public interface TripleStore {
-    public TripleStoreConnection getConnection();
-    public TripleStoreConnection getConnection(String graph);
-}
